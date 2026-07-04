66b3e05711c984527da432e5eaa3087e41b980fd
Brb2004 <braydenbenard7@gmail.com>
2026-07-03 22:07:05 -0500

    Register all native modules (input, math, core), not just GL

--- src/natives/natives.c
+++ src/natives/natives.c
@@ -1,6 +1,13 @@
 #include "natives.h"
- void registerGLNatives();
+
+void registerGLNatives();
+void registerInputNatives();
+void registerMathNatives();
+void registerCoreNatives();
+
 void registerNatives() {
     registerGLNatives();
-    
+    registerInputNatives();
+    registerMathNatives();
+    registerCoreNatives();
 }
